ce1100ae341c6cc3994447f9f986774582e54c57
Geert Barentsen <geert@barentsen.be>
2013-04-30 20:13:17 +0100

    Added support for partnerID and magnitudes

--- dr2/seaming/qsub-seaming.sh
+++ dr2/seaming/qsub-seaming.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# Crowded fields need more memory to run
-for LON in `seq 25 5 100`
+
+# Crowded regions need more memory to run
+for LON in `seq 25 5 115`
 do
     qsub -N seaming$LON -v STRIP=$LON -l pmem=4gb pbs-seaming.sh
 done
 
-for LON in `seq 110 5 215`
+# Less crowded regions
+for LON in `seq 120 5 215`
 do
     qsub -N seaming$LON -v STRIP=$LON -l pmem=2gb pbs-seaming.sh
 done
