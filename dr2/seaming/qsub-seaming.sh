b7f8d4b130b19e977d68e3a83084dcdc305f5e79
Geert Barentsen <geert@barentsen.be>
2013-05-01 11:07:35 +0100

    Tunes seaming PBS config

--- dr2/seaming/qsub-seaming.sh
+++ dr2/seaming/qsub-seaming.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Crowded regions need more memory to run
-for LON in `seq 25 5 115`
+for LON in `seq 25 5 100`
 do
-    qsub -N seaming$LON -v STRIP=$LON -l pmem=3gb,walltime=08:00:00 pbs-seaming.sh
+    qsub -N seaming$LON -v STRIP=$LON -l pmem=4gb,walltime=08:00:00 pbs-seaming.sh
 done
 
 # Less crowded regions
-for LON in `seq 120 5 215`
+for LON in `seq 105 5 215`
 do
     qsub -N seaming$LON -v STRIP=$LON -l pmem=2gb,walltime=06:00:00 pbs-seaming.sh
 done
