809554a174b118d84db0e39e5a44c6939ac9f57a
Geert Barentsen <geert@barentsen.be>
2013-05-09 18:02:55 +0100

    bandmerging comments

--- scripts/qsub-bandmerging.sh
+++ scripts/qsub-bandmerging.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Executes the band-merging script
+# distributed by splitting up the work in 10-deg longitude strips
 for LON1 in `seq 20 10 210`
 do
     LON2=$(expr $LON1 + 10)
@@ -6,8 +8,4 @@ do
 done
 
 #qsub -N bandmerging1 -v LON1=20,LON2=40 pbs-bandmerging.sh
-#qsub -N bandmerging2 -v LON1=40,LON2=60 pbs-bandmerging.sh
-#qsub -N bandmerging3 -v LON1=60,LON2=90 pbs-bandmerging.sh
-#qsub -N bandmerging4 -v LON1=90,LON2=120 pbs-bandmerging.sh
-#qsub -N bandmerging5 -v LON1=120,LON2=170 pbs-bandmerging.sh
-#qsub -N bandmerging6 -v LON1=170,LON2=220 pbs-bandmerging.sh
+
