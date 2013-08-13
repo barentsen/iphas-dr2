76a818d10f559a8851a9d0b2758cbfe34fe0646c
Geert Barentsen <geert@barentsen.be>
2013-08-13 19:35:56 +0100

    Trust the exposure times of some 9.5s i-band anchors

--- pipeline.py
+++ pipeline.py
@@ -55,16 +55,16 @@ client[:].execute('reload(concatenating)', block=True)
 Pipeline starts here
 """
 # Create an index of all single-band catalogues
-detections.create_index(cluster)  # produces 'runs.csv'
+#detections.create_index(cluster)  # produces 'runs.csv'
 
 # Enforce zp(Halpha) = zp(r) - 3.14
 #detections.sanitise_zeropoints()  # produces 'zeropoint-overrides.csv'
 
 # Convert the single-band catalogues from CASU into our own catalogue format
-#detections.convert_catalogues(cluster)  # produces 'detected/nnnnnnn_det.fits'
+detections.convert_catalogues(cluster)  # produces 'detected/nnnnnnn_det.fits'
 
 # Bandmerge all runs obtained at the same epoch and pointing
-#bandmerging.bandmerge(cluster)  # produces 'bandmerged/nnnn.fits'
+bandmerging.bandmerge(cluster)  # produces 'bandmerged/nnnn.fits'
 
 # Compute the magnitude offsets between all runs; necessary for re-calibration
 offsets.compute_offsets(cluster)  # produces 'offsets-{r|i|ha}.csv'
@@ -78,8 +78,8 @@ offsets.compute_offsets(cluster)  # produces 'offsets-{r|i|ha}.csv'
 # Identify duplicate detections where multiple pointings overlap ('seams');
 # this requires up to 4 GB RAM per process,
 # hence we only use only every fourth cluster. node (each having 1 GB).
-cluster_highmem = client[::4]
-seaming.seam(cluster_highmem)  # produces 'seamed/nnnn.fits'
+#cluster_highmem = client[::4]
+#seaming.seam(cluster_highmem)  # produces 'seamed/nnnn.fits'
 
 # Finally, concatenate the individual pointings into a single catalogue 
-concatenating.concatenate(cluster_highmem)
+#concatenating.concatenate(cluster_highmem)
