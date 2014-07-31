ac6ee536ba209206ba9560adfb87456e2e609f4e
Geert Barentsen <geert@barentsen.be>
2014-07-31 09:59:28 +0100

    Fine-tuned astrometry section

--- paper/figures/astrometry/astrometry.py
+++ paper/figures/astrometry/astrometry.py
@@ -1,19 +1,14 @@
+"""Plots a histogram showing positional residuals between IPHAS and UCAC4."""
 import pylab as p
 from astropy.table import Table
 
-
-t = Table.read('smallmatch.fits')
+t = Table.read('iphas-x-ucac4.fits')
 
 p.figure()
-p.hist(t['angDist'], range=[0.0, 0.5],
-                   bins=50, 
-                   lw=1.0,
-                   color='#dddddd')
+p.hist(t['Separation']*3600., range=[0.0, 0.5], bins=50, lw=1.0, color='#dddddd')
 p.minorticks_on()
-                             
-
 p.xlim([0,0.5])
-p.xlabel('{\sc iphas} - {\sc ucac4} position offset [arcsec]')
-p.ylabel('Stars')
-#tight_layout(pad=0.5)
+p.xlabel('Astrometric residuals against UCAC4 [arcsec]', fontsize=9)
+p.ylabel('Stars', fontsize=9)
+p.tight_layout(pad=0.5)
 p.savefig('astrometry.pdf')
